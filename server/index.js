51e097d3b4ff7a665d039225a9b78314e7a8dbf5
Baba Kasse <babakasse@yahoo.fr>
2018-06-01 14:32:36 +0200

    last modif working

--- server/index.js
+++ server/index.js
@@ -1,14 +1,104 @@
 const path = require("path");
 const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
 const uuidv4 = require("uuid/v4");
+const redis = require("redis");
+const redisPublisher = redis.createClient();
+const redisSubscriber = redis.createClient();
 
 const app = express();
 
 const PUBLIC_FOLDER = path.join(__dirname, "../public");
 const PORT = process.env.PORT || 5000;
 
-// Completer ce fichier
+const socketsPerChannels /* Map<string, Set<WebSocket>> */ = new Map();
+const channelsPerSocket /* WeakMap<WebSocket, Set<string> */ = new WeakMap();
 
+// Initialize a simple http server
+const server = http.createServer(app);
+
+// Initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+/*
+ * Subscribe a socket to a specific channel.
+ */
+function subscribe(socket, channel) {
+    let socketSubscribed = socketsPerChannels.get(channel) || new Set();
+    let channelSubscribed = channelsPerSocket.get(socket) || new Set();
+
+    socketSubscribed = socketSubscribed.add(socket);
+    channelSubscribed = channelSubscribed.add(channel);
+
+    socketsPerChannels.set(channel, socketSubscribed);
+    channelsPerSocket.set(socket, channelSubscribed);
+
+    if (socketSubscribed.size === 1) {
+        redisSubscriber.subscribe(channel);
+    }
+}
+
+/*
+ * Unsubscribe a socket from a specific channel.
+ */
+function unsubscribe(socket, channel) {
+    let socketSubscribed = socketsPerChannels.get(channel) || new Set();
+    let channelSubscribed = channelsPerSocket.get(socket) || new Set();
+
+    socketSubscribed.delete(socket);
+    channelSubscribed.delete(channel);
+
+    socketsPerChannels.set(channel, socketSubscribed);
+    channelsPerSocket.set(socket, channelSubscribed);
+
+    if (socketSubscribed.size === 0) {
+        redisSubscriber.unsubscribe(channel);
+    }
+}
+
+/*
+ * Subscribe a socket from all channels.
+ */
+function unsubscribeAll(socket) {
+    const channelSubscribed = channelsPerSocket.get(socket) || new Set();
+
+    channelSubscribed.forEach(channel => {
+        unsubscribe(socket, channel);
+    });
+}
+
+function broadcast(channel, data) {
+    redisPublisher.publish(channel, data);
+}
+
+redisSubscriber.on("message", (channel, message) => {
+    const socketSubscribed = socketsPerChannels.get(channel) || new Set();
+
+    socketSubscribed.forEach(client => {
+        client.send(message);
+    });
+});
+
+// Broadcast message from client
+wss.on("connection", ws => {
+    ws.on("close", () => {
+        unsubscribeAll(ws);
+    });
+
+    ws.on("message", data => {
+        const message = JSON.parse(data.toString());
+
+        switch (message.type) {
+            case "subscribe":
+                subscribe(ws, message.channel);
+                break;
+            default:
+                broadcast(message.channel, data);
+                break;
+        }
+    });
+});
 
 // Assign a random channel to people opening the application
 app.get("/", (req, res) => {
@@ -24,3 +114,7 @@ app.get("/:channel", (req, res, next) => {
 });
 
 app.use(express.static(PUBLIC_FOLDER));
+
+server.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${server.address().port}`);
+});
